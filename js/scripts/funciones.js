f46d7660f86acadebab25f76f82401615b97cc97
AgustinBroggi <broggiagustin13@outlook.com.ar>
2022-04-08 18:51:55 -0300

    Trabajo Practico 2

---
+++ js/scripts/funciones.js
@@ -0,0 +1,12 @@
+//Funciones de javascript//
+function hola(){
+    console.log('Hola Agustin')
+}
+
+function chau(){
+    console.log('Chau y boca campeon')
+}
+
+function color(){
+    alert('Hola crack')
+}
